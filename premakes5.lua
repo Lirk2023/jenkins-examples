4e90f7ee1859e42c540eab72b1628d72ef04cc68
Lirk2023 <142889821+Lirk2023@users.noreply.github.com>
2023-09-12 09:28:56 +0800

    Add files via upload

---
+++ premakes5.lua
@@ -0,0 +1,17 @@
+workspace "HelloWorld"
+	configurations { "Debug", "Release" }
+project "HelloWorld"
+	location "HelloWorld"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir "bin/%/{cfg.buildcfg}"
+
+	files { "HelloWorld/src/**.h", "HelloWorld/src/**.cpp" }
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		sysbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
